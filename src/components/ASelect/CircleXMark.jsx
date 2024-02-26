5bcaf005b76c1e05841db6486e023750c840a981
Andruraj Amalraj <aamalraj@plexustg.com>
2024-02-26 11:15:52 +0530

    svg update

--- src/components/ASelect/CircleXMark.jsx
+++ src/components/ASelect/CircleXMark.jsx
@@ -4,7 +4,7 @@ export const CircleXMark = ({ className }) => (
   <svg
     className={className}
     xmlns="http://www.w3.org/2000/svg"
-    enable-background="new 0 0 24 24"
+    enableBackground="new 0 0 24 24"
     viewBox="0 0 24 24"
     id="times-circle"
   >
